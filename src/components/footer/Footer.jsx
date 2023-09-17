50d8f4a476f24701d40df0f7e237a3ec913a50d9
Sofia Baute <91097658+SoofiBaute@users.noreply.github.com>
2023-09-17 18:33:26 -0300

    add style and image

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,15 @@
-
+import img from '../../assets/githubIcon.png';
+import './style.css';
 
 export const Footer = () => {
+
   return (
-    <div>Footer</div>
+    <div className="div">
+      <h2>Acerca de</h2>
+      <img src={img} />
+      <hr/>
+      <p>©2023 NUMENTech. Todos los derechos reservados</p>
+    </div>
   )
 }
+
