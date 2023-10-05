6e7604a0d537d40897fc565c3c8aa605eaacc53b
Martina Ruiz <martu-ruiz@hotmail.com>
2023-10-04 22:07:10 -0300

    finished mobile home section

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <div className='banner-sm'>
+      <div className='texto'>
+        <h1 className='pearl-white' >Descrubrí la nueva línea Pearl White</h1>
+        <p className='subtitle' >Los mejores productos ahora en color blanco. Disponibles a partir de Noviembre de 2023.</p>
+        <button className='boton'>Ver Más</button>
+      </div>
+    </div>
+  )
+}
+
+export default Home
