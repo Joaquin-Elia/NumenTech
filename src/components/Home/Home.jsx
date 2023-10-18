f54e3de081907fdd25bc340a9f177c25cb56a535
martinaruizdev <martinaruizdev@gmail.com>
2023-10-18 20:43:16 -0300

    finished Home section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,11 +3,11 @@ import "./Home.css"
 
 const Home = () => {
   return (
-    <div className='banner-sm'>
-      <div className='texto'>
-        <h1 className='pearl-white' >Descrubrí la nueva línea Pearl White</h1>
-        <p className='subtitle' >Los mejores productos ahora en color blanco. Disponibles a partir de Noviembre de 2023.</p>
-        <button className='boton'>Ver Más</button>
+    <div className='home-banner'>
+      <div className='home-texto'>
+        <h1 className='home-pearl-white' >Descrubrí la nueva línea Pearl White</h1>
+        <p className='home-subtitle' >Los mejores productos ahora en color blanco. Disponibles a partir de Noviembre de 2023.</p>
+        <button className='home-boton'>Ver Más</button>
       </div>
     </div>
   )
